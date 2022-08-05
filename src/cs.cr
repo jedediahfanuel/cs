79a31db54c277541ad6f14df468bdf82fdfd04da
Jeddi212 <jedediahf51@gmail.com>
2022-08-05 18:10:45 +0700

    refactor: table coloring, many refactor code point

--- src/cs.cr
+++ src/cs.cr
@@ -20,13 +20,11 @@ OptionParser.parse do |parser|
   end
   
   parser.on "-p PATH", "--path PATH", "Specify the path" do |path| 
-    test_path(path)
-    Var.path = path unless path.empty?
-    Var.reload_curd
+    NPath.update_path(path)
   end
   
   parser.on "-s", "--slash", "Append slash on directory" do
-    Format.append_slash
+    Format.is_slash = true
   end
   
   parser.on "-t", "--tab", "Use tabular output" do
@@ -34,18 +32,14 @@ OptionParser.parse do |parser|
   end
 end
 
+if Format.is_slash
+  Format.append_slash
+end
+
 if Format.is_table
-  Format.tabular_output
-  # print Var.table.map(&.map(&.ljust(20, ' ')).join(" ")).join("\n")
-  print Var.table.map { |a| 
-    a.map_with_index { |col, i| 
-      col.matches?(/\D/) ? col.ljust(Var.just[i], ' ') : col.rjust(Var.just[i], ' ')
-    }.join(" ")
-  }.join("\n")
-elsif Format.is_color
-  Format.load_color
-  print Var.curdc.join(" ")
+  Format.tabular
+  PrintOut.tabular
 else
-  print Var.curd.join(" ")
+  PrintOut.liner
 end
 
