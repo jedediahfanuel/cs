66f0da3cf21726b6fbdfb27226f193861d945f23
Jeddi212 <jedediahf51@gmail.com>
2022-08-15 13:52:51 +0700

    🔧 ++ crystal tool format

--- src/util/npath.cr
+++ src/util/npath.cr
@@ -4,7 +4,7 @@ require "./var"
 module NPath
   def self.test_path(path : String)
     path = path.strip.sub('~', Path.home)
-    
+
     if Dir.exists?(path)
       path
     else
@@ -12,11 +12,10 @@ module NPath
       exit(1)
     end
   end
-  
+
   def self.update_path(path : String)
     path = test_path(path)
     Var.path = path unless path.empty?
     Var.reload_curd
   end
 end
-
--- src/util/var.cr
+++ src/util/var.cr
@@ -1,28 +1,55 @@
-module Var 
-  @@os_unix : Bool          = Path.new(@@path).windows?
-  @@path    : String        = Dir.current
-  
-  @@curd    : Array(String) = Dir.entries(@@path).sort_by! { |str| str.downcase }
-  
-  @@table  = [] of Array(String)
-  @@just = Array(Int8).new(6, 0_i8)
-  
-  @@header = ["Permissions", "Group", "User", "Size", "Modified", "Name"]
-  
-  def self.os_unix               ; @@os_unix end
-  
-  def self.path                  ; @@path end
-  def self.path=(_path : String) ; @@path = _path end
-  
-  def self.curd                  ; @@curd end
-  def self.reload_curd           ; @@curd = Dir.entries(@@path).sort_by! { |str| str.downcase } end
-  def self.curd=(_path : String) ; @@curd = Dir.entries(_path).sort_by! { |str| str.downcase } end
-  def self.curd=(ary  : Array(String)) ; @@curd = ary end
-  
-  def self.table                    ; @@table end
-  def self.just                     ; @@just  end
-  def self.just=(ary : Array(Int8)) ; @@just = ary end
-  
-  def self.header                   ; @@header end
-end
+module Var
+  @@os_unix : Bool = Path.new(@@path).windows?
+  @@path : String = Dir.current
 
+  @@curd : Array(String) = Dir.entries(@@path).sort_by! { |str| str.downcase }
+
+  @@table = [] of Array(String)
+  @@just = Array(Int8).new(6, 0_i8)
+
+  @@header = ["Permissions", "Group", "User", "Size", "Modified", "Name"]
+
+  def self.os_unix
+    @@os_unix
+  end
+
+  def self.path
+    @@path
+  end
+
+  def self.path=(_path : String)
+    @@path = _path
+  end
+
+  def self.curd
+    @@curd
+  end
+
+  def self.reload_curd
+    @@curd = Dir.entries(@@path).sort_by! { |str| str.downcase }
+  end
+
+  def self.curd=(_path : String)
+    @@curd = Dir.entries(_path).sort_by! { |str| str.downcase }
+  end
+
+  def self.curd=(ary : Array(String))
+    @@curd = ary
+  end
+
+  def self.table
+    @@table
+  end
+
+  def self.just
+    @@just
+  end
+
+  def self.just=(ary : Array(Int8))
+    @@just = ary
+  end
+
+  def self.header
+    @@header
+  end
+end
