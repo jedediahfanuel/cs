cd43de52893419b7c18b31aedd7d7e62887fa4ea
Jeddi212 <jedediahf51@gmail.com>
2022-08-26 12:25:30 +0700

    🆙 :: migrate to grid v0.1.1 & fix coloring

--- src/util/print_out.cr
+++ src/util/print_out.cr
@@ -28,14 +28,41 @@ module PrintOut
 
   def self.grid
     grid = Grid.new(Var.curd)
-    grid.virtual_generate(Term::Screen.width)
-    grid.virtual_to_canvas
-    final = grid.to_s
-
-    print final
+    grid.auto(Term::Screen.width)
+    print grid.to_s_enhancement
   end
   
   def self.path_header(_path : String)
     print "#{Format.path_header(_path)}: \n"
+  end
+end
+
+# Extending from grid library :: adding color to directory
+struct Grid
+  def to_s_enhancement(top_down : Bool = true, align_left : Bool = true, sep : String = @separator) : String
+    sep = sep[0..(@separator.size-1)] if @separator.size < sep.size
+    String.build do |io|
+      if top_down
+        return "" if @canvas_td.empty?
+        @canvas_td.first.size.times do |row|
+          @col_width_td.each_with_index do |w, col|
+            next unless @canvas_td[col][row]?
+            io << Format.load_color(align_left ? @canvas_td[col][row].ljust(w, ' ') : @canvas_td[col][row].rjust(w, ' ')) if Format.is_color
+            io << (align_left ? @canvas_td[col][row].ljust(w, ' ') : @canvas_td[col][row].rjust(w, ' ')) unless Format.is_color
+            io << sep if col < (@col_width_td.size - 1)
+          end
+          io << "\n"
+        end
+      else
+        return "" if @canvas_lr.empty?
+        @canvas_lr.each do |row|
+          row.each_with_index do |str, i|
+            io << (align_left ? str.ljust(@col_width_lr[i], ' ') : str.rjust(@col_width_lr[i], ' '))
+            io << sep if i < (row.size - 1)
+          end
+          io << "\n"
+        end
+      end
+    end
   end
 end
